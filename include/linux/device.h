2fdf304690230ebfb40ba0da412d12110b2b3116
threewater <threewaterl@163.com>
2015-02-24 23:37:39 +0800

    patch deferred_initcalls and used it for imx6 mmc to save boot time

--- include/linux/device.h
+++ include/linux/device.h
@@ -1212,4 +1212,16 @@ static void __exit __driver##_exit(void) \
 } \
 module_exit(__driver##_exit);
 
+#define deferred_module_driver(__driver, __register, __unregister, ...) \
+static int __init __driver##_init(void) \
+{ \
+	return __register(&(__driver) , ##__VA_ARGS__); \
+} \
+deferred_module_init(__driver##_init); \
+static void __exit __driver##_exit(void) \
+{ \
+	__unregister(&(__driver) , ##__VA_ARGS__); \
+} \
+module_exit(__driver##_exit);
+
 #endif /* _DEVICE_H_ */
